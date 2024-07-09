fbde84e9407ad866350e8deac514e9b2803461c4
KR-EGOIST <jhso5041@naver.com>
2024-07-09 12:01:42 +0900

    ♻️ refactor : 해설강의 데드 레커닝 프레임별 계산 방법 수정

--- src/classes/models/user.class.js
+++ src/classes/models/user.class.js
@@ -37,11 +37,10 @@ class User {
   // 추측항법을 사용하여 위치를 추정하는 메서드
   // 본인의 x, y위치는 각 user 클래스마다 가지고 있으므로 각자 계산한 뒤 나중에 모아서 보내면 된다.
   calculatePosition() {
-    const timeDiff = this.latency / 1000; // 레이턴시(ms)를 초(s) 단위로 계산
+    const timeDiff = (Date.now() - this.lastUpdateTime + this.latency) / 1000; // 레이턴시(ms)를 초(s) 단위로 계산
     const speed = 3; // 클라이언트의 속도가 3으로 설정되어있음
-    const frame = 1 / 30; // 클라이언트가 30 fps 를 가지고 있음
     // distance(거리) = speed(속도) * timeDiff(시간)
-    const distance = speed * frame + speed * frame * timeDiff;
+    const distance = speed * timeDiff;
 
     this.x = this.x + distance * this.directionX;
     this.y = this.y + distance * this.directionY;
